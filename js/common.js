22cb99832b1baea156ec06acd33e4e38573d2488
richard omollo <romollo@romollos-iMac.local>
2014-12-10 13:35:11 +0300

    Mobile menu added first commit

--- js/common.js
+++ js/common.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {	
 	$('#top-wrapper').waypoint('sticky');
+	$('.popmenu-prompt').click(function(event) {
+		/* Act on the event */
+		$('.menu-overlay').fadeToggle();
+		$('.popmenu-prompt').fadeOut('fast', function() {
+			$('.close-popmenu-prompt').fadeIn();
+		});
+		
+	});
+
+	$('.close-popmenu-prompt').click(function(event) {
+		/* Act on the event */
+		$('.menu-overlay').fadeToggle();
+		$('.close-popmenu-prompt').fadeOut('fast', function() {
+			$('.popmenu-prompt').fadeIn();
+		});
+		
+	});
 });
 
 $(window).load(function() {
